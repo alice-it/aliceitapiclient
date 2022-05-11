e3cdd2c6d6e836f5cc973fa437391a4fffb4ba7d
Schmolke <51118451+Schmolke@users.noreply.github.com>
2022-05-11 18:08:38 +0200

    Update SingleCloudServerHandler.php

--- src/SingleCloudServerHandler.php
+++ src/SingleCloudServerHandler.php
@@ -164,4 +164,32 @@ class SingleCloudServerHandler
             ]
         );
     }
+    
+    /**
+     * @return mixed
+     */
+    public function listBackups(){
+        return $this->client->get($this->basPath.'/backups');   
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function createBackup(){
+        return $this->client->post($this->basPath.'/backups');   
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function restoreBackup($backup_id){
+        return $this->client->post($this->basPath.'/backups/'.$backup_id);   
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function deleteBackup($backup_id){
+        return $this->client->delete($this->basPath.'/backups/'.$backup_id);   
+    }
 }
